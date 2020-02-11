bd08780a9f254dc042640119b1d42af67fabd072
DubocSophia <50839405+DubocSophia@users.noreply.github.com>
2020-02-11 20:19:44 -0300

    pastas separadas por tipo de acesso

---
+++ src/java/filtros/FiltroAdmin.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filtros;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author sophi
+ */
+public class FiltroAdmin implements Filter {
+
+    public FiltroAdmin() {
+    }
+
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpSession session = httpRequest.getSession(false);
+        if (session != null) {
+            if (session.getAttribute("tipoSession") != null) {
+                if (session.getAttribute("tipoSession").equals("adm")) {
+                    chain.doFilter(request, response);
+                    return;
+                }
+            }
+        }
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.sendRedirect("../../index.html");
+    }
+
+    public void destroy() {
+    }
+
+    /**
+     * Init method for this filter
+     */
+    public void init(FilterConfig filterConfig) {
+
+    }
+
+}
